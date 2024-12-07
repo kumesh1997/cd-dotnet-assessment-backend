1810d0a8c26ebb29fdbe04062a6efe414b311099
TharinduKumesh <TharinduKumesh@GRS14844>
2024-12-07 22:09:27 +0530

    Custom healthcheck, mapping config added, programe.cs updated with cors, health check and error middlewares

---
+++ Assessment.Api/CustomConfigurations/CustomHealthCheck.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Assessment.Api.CustomConfigurations
+{
+    public class CustomHealthCheck : IHealthCheck
+    {
+        private readonly string _environmentName;
+
+        public CustomHealthCheck(string environmentName)
+        {
+            _environmentName = environmentName;
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            string message = $"API is Up and Running in {_environmentName} environment";
+            return Task.FromResult(HealthCheckResult.Healthy(message));
+        }
+    }
+}
